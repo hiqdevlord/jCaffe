decc4cc2dddc8fbda855bc5dc132ab0b47b57fef
hiqdevlord <hiqdevlord@gmail.com>
2015-04-21 17:56:52 -0400

    First iteration of exception handling is done

--- src/jni/jSolver.cpp
+++ src/jni/jSolver.cpp
@@ -1,17 +1,15 @@
 #include <string>
+// #include <setjmp.h>
 
-//#include "jni_exception.h"
 #include "util.hpp"
 #include "edu_h2r_jSolver.h"
 
 using caffe::Solver;
-using caffe::LayerParameter;
 
 
 JNIEXPORT jlong JNICALL Java_edu_h2r_jSolver_createSolver(JNIEnv *env,
                                             jobject obj, jstring solverFile) {
-    //SAVE_PGM_STATE();
-    //JNI_ASSERT(0, "Coucou");
+
     const char* cSolverFile = env->GetStringUTFChars(solverFile, NULL);
 
     caffe::SolverParameter solver_param;
@@ -29,15 +27,11 @@ JNIEXPORT jlong JNICALL Java_edu_h2r_jSolver_createSolver(JNIEnv *env,
     }
 
     Solver<float> *solver = caffe::GetSolver<float>(solver_param);
+    JNI_ASSERT(solver != NULL, "caffe::GetSolver returned a null pointer");
+
     setInternalPtr<Solver<float> >(env, obj, solver);
 
     env->ReleaseStringUTFChars(solverFile, cSolverFile);
-
-    // Send the input scale back to java
-    caffe::NetParameter net_param;
-    caffe::ReadProtoFromTextFileOrDie(solver_param.net(), &net_param);
-    const LayerParameter& layer_param = net_param.layer(0);
-    env->SetFloatField(obj, getObjField(env, obj, "inputScale", "F"), layer_param.transform_param().scale());
 
     return reinterpret_cast<jlong>(solver);
 }
